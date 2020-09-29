564df42fe4f134e1c32c8c7fc8ad3fa72a125d3c
steadman1 <64872573+steadman1@users.noreply.github.com>
2020-09-29 07:21:40 -0400

    Update steadman_digital.py

--- steadman.digital/steadman_digital.py
+++ steadman.digital/steadman_digital.py
@@ -14,4 +14,4 @@ def class_trans():
     return render_template("class-transcript.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='steadman-digital.herokuapp.com')
